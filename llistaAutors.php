38709c7a376cc4f429a8618fd57d579bd69a1a21
unknown <fxramonfeina@gmail.com>
2018-11-27 20:50:46 +0100

    Acabat, falta ajustar

--- llistaAutors.php
+++ llistaAutors.php
@@ -12,24 +12,86 @@ and open the template in the editor.
     <body>
         <h1>Algo</h1>
         <?php
+        $perid = isset($_POST['idcercar']) ? $_POST['idcercar'] : "";
+        $pernom = isset($_POST['nomcercar']) ? $_POST['nomcercar'] : "";
+        $cercarper = isset($_POST['cercarper']) ? $_POST['cercarper'] : "1";
+        $mentres = isset($_POST['mentres']) ? $_POST['mentres'] : "ID_AUT";
+        //$pagina = isset($_POST['pagina']) ? $_POST['pagina'] : "ID_AUT";
+
         $mysqli = new mysqli("localhost", "xavier", "1234", "biblioteca");
         $mysqli->set_charset("utf8");
-        $contador = 0;
-        if (!empty($_POST["seguent"])) {
-            $contador = $_POST["contador"] + 10;
-            $query = ("SELECT * FROM AUTORS LIMIT 10 OFFSET " . $contador);
-        } elseif (!empty($_POST["anterior"])) {
-            $contador = $_POST["contador"] - 10;
-            $query = ("SELECT * FROM AUTORS LIMIT 10 OFFSET " . $contador);
-        } elseif (!empty($_POST["fi"])) {
-            $contador = 0;
-            $query = ("SELECT * FROM AUTORS order by ID_AUT desc LIMIT 10");
-        } elseif (!empty($_POST["inici"])) {
-            //$contador = $_POST["contador"] - 10;
-            $query = ("SELECT * FROM AUTORS order by ID_AUT asc LIMIT 10");
-        } else {
-            $query = ("SELECT * FROM AUTORS LIMIT 10");
+        $pagina = 1;
+        $vinici = 0;
+        $vfi = 20;
+        $quantitat = "";
+        $ordenat = "ID_AUT";
+        $ordre = "asc";
+        $queryFi = "select count(*) as 'quantitat' from AUTORS";
+        if ($cursor = $mysqli->query($queryFi)) {
+            while ($row = $cursor->fetch_assoc()) {
+                $quantitat = $row['quantitat'];
+            };
+            $npagines = ceil($quantitat / 20);
+            $npagines -= 1;
+            $cursor->free();
+        };
+
+        if (isset($_POST["pagina"])) {
+            $pagina = $_POST["pagina"];
         }
+        if (isset($_POST["ordre"]) && isset($_POST["ordre"])) {
+            $ordre = $_POST["ordre"];
+            $ordenat = $_POST["ordenat"];
+        } else {
+            $ordre = "asc";
+            $ordenat = "ID_AUT";
+        }
+        if (!empty($_POST["nasc"])) {
+            $ordenat = "NOM_AUT";
+            $ordre = "asc";
+        }
+        if (!empty($_POST["ndesc"])) {
+            $ordenat = "NOM_AUT";
+            $ordre = "desc";
+        }
+        if (!empty($_POST["casc"])) {
+            $ordenat = "ID_AUT";
+            $ordre = "desc";
+        }
+        if (!empty($_POST["cdesc"])) {
+            $ordenat = "ID_AUT";
+            $ordre = "asc";
+        }
+        if (!empty($_POST["seguent"])) {
+            if ($pagina != $npagines) {
+                $pagina++;
+            };
+        } elseif (!empty($_POST["anterior"])) {
+            if ($pagina > 0) {
+                $pagina--;
+            };
+        } elseif (!empty($_POST["inici"])) {
+            $pagina = 0;
+        } elseif (!empty($_POST["fi"])) {
+            $pagina = $npagines;
+        }
+        $vinici = $pagina * $vfi;
+        if (!empty($_POST["cercar"])) {
+            if (!empty($pernom)) {
+                $cercarper = "'%" . $pernom . "%'";
+                $mentres = "NOM_AUT";
+            } else if (!empty($perid)) {
+                $cercarper = $perid;
+                $mentres = "ID_AUT";
+            } else {
+                $cercarper = "1";
+                $mentres = "ID_AUT";
+            }
+            $query = "select * from AUTORS where " . $mentres . " like " . $cercarper . " order by " . $ordenat . " " . $ordre . " limit " . $vinici . "," . $vfi;
+        } else {
+            $query = "select * from AUTORS order by " . $ordenat . " " . $ordre . " limit " . $vinici . "," . $vfi;
+        }
+
         echo "<table  border = \"1\">";
         echo "<tr>";
         echo "<th>ID</th>";
@@ -48,7 +110,20 @@ and open the template in the editor.
         $mysqli->close();
         ?>
         <form action="llistaAutors.php" method="post" id="filtres" >
-            <input type="hidden" name="contador" value="<?= $contador ?>">
+            <input type="hidden" name="pagina" value="<?= $pagina ?>">
+            <input type="hidden" name="ordenat" value="<?= $ordenat ?>">
+            <input type="hidden" name="ordre" value="<?= $ordre ?>">
+            <input type="hidden" name="cercarper" value="<?= $cercarper ?>">
+            <input type="hidden" name="mentres" value="<?= $mentres ?>">
+            Cercar per ID:
+            <input type="text" name="idcercar">
+            Cercar per Nom:
+            <input type="text" name="nomcercar">
+            <input type="submit" value="Cercar" name="cercar"></br>
+            <input type="submit" value="NOM" name="nasc">
+            <input type="submit" value="nom" name="ndesc">
+            <input type="submit" value="CODI" name="casc">
+            <input type="submit" value="codi" name="cdesc"></br>
             <input type="submit" value="Inici" name="inici">
             <input type="submit" value="Anterior" name="anterior">
             <input type="submit" value="Següent" name="seguent">
@@ -56,3 +131,4 @@ and open the template in the editor.
         </form>
     </body>
 </html>
+
